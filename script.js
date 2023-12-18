5f8cd2d23cd7b602345941848408b5bc49388882
Bl0xxy <99568449+Bl0xxy@users.noreply.github.com>
2023-12-18 15:49:20 -0600

    removed debug function from script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function toggleSection(sectionId) {
-    print("Toggle Section Ran with section id of " + sectionId);
     const section = document.getElementById(sectionId);
     section.classList.toggle('revealed');
 }
